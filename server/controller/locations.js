4a8f641a909b93a86c49d0d062766585afe2770f
jayyrt <jayy.rt19@gmail.com>
2021-12-18 16:57:02 -0700

    locations controller created

--- server/controller/locations.js
+++ server/controller/locations.js
@@ -1,8 +1,10 @@
 module.exports = {
     getAllLocations: async (req, res) => {
-    //   const locations = await req.app.get('db').
+    allLocations = await req.app.get('db').get_locations();
+    return res.status(200).send(allLocations);
     },
-    getAttractions: (req, res) => {
-        
+    getAttractions: async (req, res) => {
+    attractions = await req.app.get('db').get_attractions();
+    return res.status(200).send(attractions);        
     }
 }
