4fe28d8b21450c52c901dbe167a6babd4228dacd
michellethompson-github <80917965+michellethompson-github@users.noreply.github.com>
2021-12-21 12:02:16 -0500

    Locations Page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Locations from './components/Locations';
+import Attractions from './components/Attractions';
+import Itinerary from './components/Itinerary';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Locations 
+      title='Tokyo'
+      imageUrl='https://media.istockphoto.com/photos/aerial-view-of-tokyo-cityscape-with-fuji-mountain-in-japan-picture-id1131743616?k=20&m=1131743616&s=612x612&w=0&h=5IzvPM791pd7-TFIB16zl1-CHgcqOBFLbQ9U1d6cUw8='
+      body='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'
+
+
+      />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import Nav from './Nav'
 const Header = () => {
     return (
         <header>
-            <h1>Header</h1>
+            <Nav/>
         </header>
     )
 }
