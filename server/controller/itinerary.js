3386d997194fbdda999365a72dc1ea695e087717
jayyrt <jayy.rt19@gmail.com>
2021-12-21 19:43:55 -0700

    completed itinerary controller

--- server/controller/itinerary.js
+++ server/controller/itinerary.js
@@ -1,11 +1,16 @@
 module.exports = {
-    addItineraryItem: (res, req) => {
-
+    addItineraryItem: async (res, req) => {
+        const { id } = req.session.user;
+        const { dayNumber, attractionId } = req.body;
+        const userItinerary = await req.app.get('db').create_itinerary([dayNumber, id, attractionId]);
+        return res.status(200).send(userItinerary);
     },
-    deleteItineraryItem: (res, req) => {
-
+    deleteItineraryItem: async (res, req) => {
+        req.app.get('db').user_itinerary.delete_itinerary(req.params.id)
+        .then(_ => res.sendStatus(200))
     },
-    getLocationItinerary: (res, req) => {
-
+    getLocationItinerary: async (res, req) => {
+        getUserItinerary = await req.app.get('db').get_user_itinerary();
+        return res.status(200).send(getUserItinerary);
     }
 }
