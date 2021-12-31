942cbe88f20df7de84665f1d4463290e52af83fe
AlexTIM13 <alejandro.partid@gmail.com>
2021-12-30 21:59:17 -0600

    fix app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import './App.css';
 import Header from './components/Header';
-<<<<<<< HEAD
-import Locations from './components/Locations';
-// import Attractions from './components/Attractions';
-// import Itinerary from './components/Itinerary';
-=======
->>>>>>> main
+
 import routes from './routes';
 
 function App() {
