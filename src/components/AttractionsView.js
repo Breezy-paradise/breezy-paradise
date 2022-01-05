3689602919169d75963fe64112a91e12a4afad2c
Cathy Richardson <cathyanne08@gmail.com>
2022-01-04 20:20:45 -0700

    deleted extra props param

--- src/components/AttractionsView.js
+++ src/components/AttractionsView.js
@@ -37,7 +37,7 @@ const AttractionsView = (props) => {
     }
 
     getData();
-    getItineraryData(props);
+    getItineraryData();
   }, [props.user]);
 
   const getItineraryData = async () => {
