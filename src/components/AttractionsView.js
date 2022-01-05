5503ea0c235d0ba9cd43edd24d816d073779d26d
Cathy Richardson <cathyanne08@gmail.com>
2022-01-04 20:07:34 -0700

    get itinerary if user is signed in

--- src/components/AttractionsView.js
+++ src/components/AttractionsView.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react'
 import { useParams } from "react-router-dom";
 import axios from 'axios'
+import { connect } from 'react-redux';
 import Attractions from '../components/Attractions'
 import AttractionDetail from '../components/AttractionDetail'
 import AttractionItinerary from '../components/AttractionItinerary'
 import attractionsDb from './attractionsDb'
 import './Attractions.css';
 
-const AttractionsView = () => {
+const AttractionsView = (props) => {
   const [attraction, setAttraction] = useState([]);
   const [location, setLocation] = useState();
   const [userItinerary, setUserItinerary] = useState([]);
@@ -25,6 +26,7 @@ const AttractionsView = () => {
 
         const locationResult = await axios.get(`/api/locations/${locationId}`);
         setLocation(locationResult.data);
+
       } catch (err) {
         if (err.isAxiosError) {
           console.log(err.response.request.responseText);
@@ -35,7 +37,38 @@ const AttractionsView = () => {
     }
 
     getData();
-  }, []);
+    getItineraryData(props);
+  }, [props.user]);
+
+  const getItineraryData = async () => {
+    try {
+      //if not logged in, clear the itinerary 
+      if (!props.user.username) {
+        setUserItinerary([]);
+        return;
+      }
+      const itineraryResult = await axios.get(`/api/itinerary/${locationId}`);
+      setUserItinerary(itineraryResult.data);
+    } catch (err) {
+      if (err.isAxiosError) {
+        console.log(err.response.request.responseText);
+      } else {
+        console.log(err);
+      }
+    }
+  }
+
+  const addAttractionToItinerary = (attraction) => {
+    // TODO: Not Yet Implemented
+    //     insert attraction to db user_itinerary table
+    getItineraryData()
+  }
+
+  const deleteItineraryItem = (itineraryId) => {
+    // TODO: Not Yet Implemented
+    // delete the itinerary item
+    getItineraryData()
+  }
 
   return (
     <div className="container">
@@ -43,23 +76,29 @@ const AttractionsView = () => {
       <div className="container1">
         <h2> {location ? location.name : ""} Attractions</h2>
         {attraction.map(attraction =>
-          <Attractions attraction={attraction} />
+          <Attractions attraction={attraction} setAttraction={setAttraction} />
         )}
       </div>
 
       {/* <div className="container2"> 
               <h2>Attractions Details</h2>  
               <h3>Statue of Liberty and Ellis Island Tour</h3>    
-                <AttractionDetail/>
+                <AttractionDetail attraction={selectedAttraction} addAttractionToItinerary={addAttractionToItinerary} />
             </div>      
 
             <div className="container3">
               <h2>Itinerary</h2>  
-                 <AttractionItinerary/> 
+                 <AttractionItinerary itinerary={userItinerary} deleteItineraryItem={deleteItineraryItem} /> 
             </div> */}
 
     </div>
   )
 }
 
-export default AttractionsView
+function mapStateToProps(state) {  // redux properties to get from store
+  return {
+    user: state.user
+  }
+}
+
+export default connect(mapStateToProps)(AttractionsView);
