63f6e657acc65b87bbf09be5ae75e9c23badc4b6
Cathy Richardson <cathyanne08@gmail.com>
2022-01-11 21:39:37 -0700

    made the addAttractionToItinerary method

--- db/user_itinerary/get_user_itinerary.sql
+++ db/user_itinerary/get_user_itinerary.sql
@@ -1,4 +1,5 @@
 SELECT * FROM user_itinerary ui
 JOIN attraction a ON a.id = ui.attraction_id
 WHERE a.location_id = $1
-AND ui.user_id = $2;
+AND ui.user_id = $2
+ORDER BY ui.day_num;
--- src/components/AttractionsView.js
+++ src/components/AttractionsView.js
@@ -57,10 +57,20 @@ const AttractionsView = (props) => {
     }
   }
 
-  const addAttractionToItinerary = (attractionId, dayNumber) => {
-    // TODO: Not Yet Implemented
-    //     insert attraction to db user_itinerary table
-    getItineraryData()
+  const addAttractionToItinerary = async (dayNumber, attractionId) => {
+    // insert attraction to db user_itinerary table
+    let body = { dayNumber, attractionId };
+    try {
+      await axios.post('/api/itinerary', body);
+      getItineraryData();
+    } catch (err) {
+      if (err.isAxiosError) {
+        console.log(err.response.request.responseText);
+        alert(err.response.request.responseText);
+      } else {
+        console.log(err);
+      }
+    }
   }
 
   const deleteItineraryItem = (itineraryId) => {
@@ -70,7 +80,7 @@ const AttractionsView = (props) => {
   }
 
   const setAttraction = (attraction) => {
-   setCurrentAttraction(attraction);
+    setCurrentAttraction(attraction);
   }
 
   return (
@@ -98,7 +108,7 @@ const AttractionsView = (props) => {
   )
 }
 
-function mapStateToProps(state) {  
+function mapStateToProps(state) {
   return {
     user: state.user
   }
