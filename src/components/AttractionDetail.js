eed91d2648cab69a4195837f7918444df82d33d0
AlexTIM13 <alejandro.partid@gmail.com>
2022-01-13 20:48:38 -0600

    new branch

--- src/components/AttractionDetail.js
+++ src/components/AttractionDetail.js
@@ -1,4 +1,5 @@
 import React from 'react'
+<<<<<<< Updated upstream
 import './Attractions.css';
 
 let attraction =
@@ -38,6 +39,30 @@ const AttractionDetail = (attraction) => {
       <h4 className="duration">Duration hrs: {attraction.duration_hours}</h4>
 
       <button className="btn">Select Attraction</button>
+=======
+import './Attractions.css'
+
+const AttractionDetail = ({ attraction }) => {
+  return (
+    <div className='subcontainer-details'>
+      <div className='subcontainer-details1'>
+        <img className='image' src={attraction.image1} />
+        <img className='image' src={attraction.image2} />
+      </div>
+      <div className='subcontainer-details2'>
+        <h4 className='price'>Price: ${attraction.price}</h4>
+        <br />
+        <div>
+          Description:
+          <br />
+          <br />
+          <p className='parrafo'>{attraction.description}</p>
+        </div>
+        <br />
+        <h4 className='duration'>Duration hrs: {attraction.duration_hours}</h4>
+        <button className='btn'>Select Attraction</button>
+      </div>
+>>>>>>> Stashed changes
     </div>
 
   </div>
