07fd43ecacac7b03d0c952c13448488406f1ad0f
AlexTIM13 <alejandro.partid@gmail.com>
2022-01-13 19:45:12 -0600

    ÂÂprops in Attraction Detail

--- src/components/AttractionDetail.js
+++ src/components/AttractionDetail.js
@@ -1,24 +1,26 @@
 import React from 'react'
 import './Attractions.css'
 
-const AttractionDetail = (attraction) => {
+const AttractionDetail = (props) => {
   return (
     <div className='subcontainer-details'>
       <div className='subcontainer-details1'>
-        <img className='image' src={attraction.image1} />
-        <img className='image' src={attraction.image2} />
+        <img className='image' src={props.attraction.image1} />
+        <img className='image' src={props.attraction.image2} />
       </div>
       <div className='subcontainer-details2'>
-        <h4 className='price'>Price: ${attraction.price}</h4>
+        <h4 className='price'>Price: ${props.attraction.price}</h4>
         <br />
         <div>
           Description:
           <br />
           <br />
-          <p className='parrafo'>{attraction.description}</p>
+          <p className='parrafo'>{props.attraction.description}</p>
         </div>
         <br />
-        <h4 className='duration'>Duration hrs: {attraction.duration_hours}</h4>
+        <h4 className='duration'>
+          Duration hrs: {props.attraction.duration_hours}
+        </h4>
         <button className='btn'>Select Attraction</button>
       </div>
     </div>
